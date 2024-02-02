bdfef361bde2318dc40f60336d95188925c8b106
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-02 18:48:45 +0000

    added some example questions to questions array

--- starter/index.js
+++ starter/index.js
@@ -4,13 +4,12 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = ['What is the title of your project?', 'Please write a description for your project', 'Are there any steps to install your project?', "Please explain the usage of your project", 'Which license should your project use?', 'Are there any rules for Contributing to your project', 'How to test your project', 'Please enter your email address for questions'
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
