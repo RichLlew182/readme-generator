8143751d0b57f9b04fa575ab404472ec7c30c48e
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-02 19:02:26 +0000

    forgot that the questions needed to be an array of objects with type/message/name etc, added the first question in this format and it is now starting to work

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const inquirer = require("inquirer");
 // const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = ['What is the title of your project?', 'Please write a description for your project', 'Are there any steps to install your project?', "Please explain the usage of your project", 'Which license should your project use?', 'Are there any rules for Contributing to your project', 'How to test your project', 'Please enter your email address for questions'
+const questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  }
+  // ,
+  // 'Please write a description for your project', 'Are there any steps to install your project?', "Please explain the usage of your project", 'Which license should your project use?', 'Are there any rules for Contributing to your project', 'How to test your project', 'Please enter your email address for questions'
 
 ];
 
@@ -24,5 +30,7 @@ const questions = ['What is the title of your project?', 'Please write a descrip
 inquirer
   .prompt(questions)
   .then((response) =>
-    response ? console.log('Success') : console.log(err)
+    response ?
+    console.log(response) :
+    console.log('You forgot to answer')
   );
