69e3b1973ccedbba7380a143b501c02230af40ac
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-02 18:57:13 +0000

    seemed to be having issues with the files being in a starter folder so I have removed them

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require('path');
+// const fs = require("fs");
+// const path = require('path');
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+// const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = ['What is the title of your project?', 'Please write a description for your project', 'Are there any steps to install your project?', "Please explain the usage of your project", 'Which license should your project use?', 'Are there any rules for Contributing to your project', 'How to test your project', 'Please enter your email address for questions'
@@ -9,12 +9,20 @@ const questions = ['What is the title of your project?', 'Please write a descrip
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+
+
+// }
 
 // function call to initialize program
-init();
+// init();
+
+inquirer
+  .prompt(questions)
+  .then((response) =>
+    response ? console.log('Success') : console.log(err)
+  );
