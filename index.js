6e307b91161b6c0549f2567484f9321881abe8f8
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-02 19:06:45 +0000

    added remaining questions to the array, some will need updating in future

--- index.js
+++ index.js
@@ -8,9 +8,42 @@ const questions = [{
     type: 'input',
     message: 'What is the title of your project?',
     name: 'title',
-  }
-  // ,
-  // 'Please write a description for your project', 'Are there any steps to install your project?', "Please explain the usage of your project", 'Which license should your project use?', 'Are there any rules for Contributing to your project', 'How to test your project', 'Please enter your email address for questions'
+  },
+  {
+    type: 'input',
+    message: 'Please write a description for your project',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Are there any steps to install your project?',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Please explain the usage of your project',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Which license should your project use?',
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Are there any rules for Contributing to your project',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'Are there any steps for testing your project',
+    name: 'testing',
+  },
+  {
+    type: 'input',
+    message: 'Please enter your email address for questions',
+    name: 'questions',
+  },
 
 ];
 
